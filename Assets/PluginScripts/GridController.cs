708542f02a371b505b12cc86818696185125994b
Pikaboi <braden.cleur@gmail.com>
2021-05-18 11:19:18 +1200

    wo

---
+++ Assets/PluginScripts/GridController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    Vector2 gridArea;
+    int cellSize;
+    ShapeType cellShape;
+
+    List<Rect> cells = new List<Rect>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Initialise the grid
+    public void Initialize(Vector2 _Area, int _Size, ShapeType _Shape)
+    {
+        gridArea = _Area;
+        cellSize = _Size;
+        cellShape = _Shape;
+
+        for(int i = 0; i < gridArea.x; i++)
+        {
+            for(int j = 0; j < gridArea.y; j++)
+            {
+                cells.Add(new Rect(new Vector2(i * cellSize, j * cellSize), new Vector2(cellSize, cellSize)));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < cells.Count; i++)
+        {
+            Vector3 center = new Vector3(cells[i].x, 0.0f, cells[i].y);
+            Vector3 size = new Vector3(cellSize, 1.0f, cellSize);
+            Gizmos.DrawWireCube(transform.position + center, size);
+        }
+    }
+}
---
+++ Assets/PluginScripts/GridNavMesh.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum ShapeType
+{
+    Square,
+    Hexagon
+}
+
+public class GridNavMesh : EditorWindow
+{
+    Vector2 gridArea;
+    int cellSize;
+    ShapeType cellShape;
+
+    [MenuItem("Grid Navigation/Grid NavMesh")]
+    static void Init()
+    {
+        GridNavMesh newMenu = (GridNavMesh)EditorWindow.GetWindow(typeof(GridNavMesh));
+        newMenu.Show();
+    }
+
+    void OnGUI()
+    {
+        gridArea = EditorGUILayout.Vector2Field("Grid Size", gridArea);
+        cellSize = EditorGUILayout.IntField("Cell Size", cellSize);
+        cellShape = (ShapeType)EditorGUILayout.EnumPopup("Cell Shape", cellShape);
+
+        if(GUILayout.Button("Create Grid"))
+        {
+            Generate();
+        }
+    }
+
+    void Generate()
+    {
+        GameObject test = new GameObject("Grid");
+        GridController newGrid = test.AddComponent(typeof(GridController)) as GridController;
+        newGrid.Initialize(gridArea, cellSize, cellShape);
+    }
+}
